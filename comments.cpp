03e0b513966b3eea16d3178ee6e71e8299154537
Dev-Costa <thiagocostadev03@gmail.com>
2024-03-22 20:33:13 -0300

    Create comments.cpp

---
+++ comments.cpp
@@ -0,0 +1,14 @@
+/*
+ * That's a multiple lines commentary
+ * My name is Thiago
+ * I'm learning C++ and Vim
+*/
+
+// That's a single line commentary
+#include <iostream> // Include stantard library
+
+int main() {
+	std::cout << "Commentary!" << std::endl; // Console out
+
+	return 0;
+}
